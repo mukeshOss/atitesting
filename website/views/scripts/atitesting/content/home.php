9e8102db41e04026c16346ae9b31133d235ba672
mukesh kumar <mukesh.kumar@osscube.com>
2017-06-19 18:20:11 +0530

    changing home page and content page

---
+++ website/views/scripts/atitesting/content/home.php
@@ -0,0 +1,100 @@
+<table cellpadding="0" cellspacing="0" id="contentAndSidebar">
+      <tbody><tr>
+        <td valign="top" align="left"><div id="content">
+            <div class="topContent">
+              <div class="clear"> </div>
+            </div>
+            <table cellpadding="0" cellspacing="0" id="tableContent">
+              <tbody><tr>
+                <td id="tableContent-leftColumn"><div class="leftContent">
+                    <style>
+    .heading123 {
+    font-family:Arial, Helvetica, sans-serif;
+    font-size:22px;
+    color:#C74E46;
+    margin-bottom:10px;
+    }
+</style>
+                    <div class="heading123">High Performance Tools Educators Need To Succeed</div>
+                    <p>ATI Nursing Education is what nurse educators need to do their jobs and do them really well. As the nation's preeminent e-learning provider, ATI offers programs that are instrumental in improving faculty, student and program outcomes. With our programs, educators have the power to augment curriculum, revamp courses and faculty development, improve student tests outcomes and lower attrition rates.</p>
+                    <p><a href="Solutions.html">Learn More</a></p>
+                  </div></td>
+                <td id="tableContent-rightColumn"><div class="rightContent">
+                    <div class="heading123">Our Customer Service Is Better Than Customer Service</div>
+                    <p>Or so we’ve been told. “Awesome.” “Wonderful." "They go above and beyond." "My ATI rep fixed a problem that wasn’t even hers to fix." Those are just a few comments from our clients about our passionate service team. Our programs and tools are renowned, but it’s our people who truly make the difference. And it shows. Right now, we’re working with more than 20,000 nurse educators, 2,100 colleges and universities nationwide and 225,000 students. And counting.</p>
+                    <p><a href="About.html">Learn More</a></p>
+                  </div></td>
+              </tr>
+            </tbody></table>
+            <div class="bottomContent">
+              <div class="clear"> </div>
+            </div>
+          </div></td>
+        <td valign="top" align="right"><div class="sidebar">
+            <div id="signOnForm" style="">
+              <input tabindex="100" id="signOnForm-userId" onfocus="JavaScript:userIdFocus(event, this);" onblur="JavaScript:userIdBlur(event, this);" onkeypress="JavaScript: return userIdKeyPress(event,'');" name="txtUserName" type="text" value="Username">
+              <div id="userNameErrorMessage" class="userId-error" style="display:none;"> </div>
+              <div id="signOnForm-passwordAndGo">
+                <input tabindex="101" id="signOnForm-passwordText" name="signOnForm-passwordText" value="Password" onfocus="JavaScript:passwordTextFocus(event, this);">
+                <input tabindex="102" id="signOnForm-password" name="txtPassword" type="password" onblur="JavaScript:passwordBlur(event, this);" onkeypress="JavaScript: return passwordKeyPress(event,'');" value="" style="display:none;">
+                <input tabindex="102" class="signOnForm-go" type="button" onclick="JavaScript: return submitSignOnForm('');" onmouseover="JavaScript: signInGoMouseOver(event, this);" onmouseout="JavaScript: signInGoMouseOut(event, this);">
+                <div class="clear"> </div>
+                <div id="passwordErrorMessage" class="password-error" style="display:none;"> </div>
+              </div>
+              <div id="signOnForm-other">
+                <div id="signOnForm-needHelp"> <a href="ati_next_gen\General\Login_Step1.htm" id="ctl00_forgotPassword" class="signOnForm-needHelp-link">Forgot username and/or password?</a> </div>
+                <div id="signOnForm-register"> <a href="" id="ctl00_register" class="signOnForm-register-link">Create an account.</a> </div>
+              </div>
+            </div>
+            <!-- ATING-823 - 2012/08/01 - JDJT - modify 'forgotPassword' text --> 
+            <script type="text/javascript">
+								function modifyLoginHelpText()
+								{
+									var myAnchors;
+									myAnchors = document.getElementsByTagName('a');
+									for (i = 0; i < myAnchors.length; i++)
+									{
+										if(myAnchors[i].id.indexOf('forgotPassword') != -1)
+										{
+											myanchor = document.getElementById(myAnchors[i].id);
+										}
+									}
+									if (myanchor && myanchor != '') 
+									{
+										myanchor.innerHTML = 'Forgot username and/or password?';
+									}
+								}
+								modifyLoginHelpText();
+							</script> 
+            
+            <!-- Start of Async HubSpot Analytics Code --> <script type="text/javascript">
+    (function(d,s,i,r) {
+      if (d.getElementById(i)){return;}
+      var n=d.createElement(s),e=d.getElementsByTagName(s)[0];
+      n.id=i;n.src='//js.hs-analytics.net/analytics/'+(Math.ceil(new Date()/r)*r)+'/497482.js';
+      e.parentNode.insertBefore(n, e);
+    })(document,"script","hs-analytics",300000);
+  </script> <!-- End of Async HubSpot Analytics Code -->
+            <div style="background-color: #ffffff; height: 7px;"></div>
+            <div class="orange">
+              <p style="font-weight: bold; font-size: 16px;">Important!</p>
+              <p>Our recommended browser is <a href="https://www.mozilla.org/en-US/firefox/all/?q=english (us)">Firefox</a>.</p>
+              <p><a href="TechnicalRequirements.html">View more technical requirements</a></p>
+            </div>
+            <style>
+    .orange p {color:#ffffff; text-align:left !important; font-size:12px;}
+    .orange { background:#eb8031; padding:15px 8px 8px 8px; }
+    .orange a {color:#ffffff; text-align:left !important; text-decoration:underline;}
+</style>
+            <div style="background-color: #ffffff; height: 7px;"></div>
+            <div class="sidebar-actionLinks"> <a href="" style="margin-bottom: 0px;">Virtual ATI Login</a></div>
+            <div style="background-color: #ffffff; height: 7px;"></div>
+            <div class="sidebar-actionLinks"><a href="ati_next_gen\customecom\SessionTypeSelection.htm?stf=1">Register for TEAS</a>&nbsp;<a href="ati_next_gen\customecom\SessionTypeSelection-1.htm?stf=7" title="PSI is our national testing center for the TEAS">Register for TEAS with PSI</a> <a shape="rect" href="ati_next_gen\customecom\SessionCityStateSelection.htm?stf=2&amp;stype=3&amp;prev=1">Register for NCLEX Live Review</a> <a shape="rect" href="">Shop Online</a> </div>
+            <script>document.write("<scr"+"ipt src='https://bp.specificclick.net?pixid=99068583&u="+escape(parent.document.location)+"&r="+escape(parent.document.referrer)+"'></scri"+"pt>");</script><script src="https://bp.specificclick.net?pixid=99068583&amp;u=file%3A///home/harpreet/Downloads/atitesting/atitesting/Home.html&amp;r="></script>
+            <noscript>
+            &lt;img src="https://bp.specificclick.net?pixid=99068583" width="0" height="0" border="0"&gt;
+            </noscript>
+            <div style="background-color: #ffffff; height: 7px;"></div>
+          </div></td>
+      </tr>
+    </tbody></table>
