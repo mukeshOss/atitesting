c851387486cddee42b5dd939f42b1103e3e0d223
Harpreet <harpreet.kaur@osscube.com>
2017-06-21 15:30:18 +0530

    jb learning templating

---
+++ website/static/jblearning/js/ie9Instructions.js
@@ -0,0 +1,23 @@
+﻿function launchIE9InstructionsDialog(){
+    $('div#dv_IE9Instructions').dialog('open');
+	return false;
+}
+
+$(function () {
+    $('div#dv_IE9Instructions').dialog({
+        bgiframe: true,
+        autoOpen: false,
+        modal: true,
+        position: 'center',
+        width: 640,
+        height: 480,
+        draggable: false,
+        resizable: false,
+        title: 'Internet Explorer 9 Compatability View',
+        buttons: {
+            OK: function () {
+                $(this).dialog('close');
+            }
+        }
+    });
+});
